b24ed51db1fd50c22f16e7659dcffd44fff4f418
filipeloose <filipeloose123@gmail.com>
2021-04-13 23:43:21 -0400

    Update produtos.service.ts

--- src/produtos/produtos.service.ts
+++ src/produtos/produtos.service.ts
@@ -14,14 +14,5 @@ export class ProdutosService {
       //return await this.produtosRepository.find();
       return await this.produtosRepository.createQueryBuilder("Produtos").select(["Produtos.cDProd, Produtos.nomeProduto"]).getMany();
     }
-    async getById(id){
-       //return await this.produtosRepository.find(id); 
-       const qb = this.produtosRepository.createQueryBuilder("Produtos");
-       
-       qb.select("Produtos.cDProd, Produtos.nomeProduto, Produtos.estoqueGeral")
-       .where("Produtos.cDProd = :id").setParameters(id);
-       console.log(qb.getSql());
-       return await qb.getRawMany();
-    }
-    //ddddddddddddddddddddd
+    
 }
